12ec6e520a37b6b3adb3db025649ba38159ae947
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-04-06 11:41:50 +0200

    fix: remove next-intl from next.config.ts

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,10 +1,8 @@
-import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from 'next';
 
-const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
   // your config here if needed
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
